599d01b6179b4f0f46afefcd289548884800b28a
Vadim Eremenko <eremenko.v.o@yandex.ru>
2026-02-19 10:27:04 +0300

    refactor(electron-client): remove deprecated files and update settings
    - Deleted obsolete files related to the Electron client, including configuration, environment, and HTML templates.
    - Updated settings to include a new npm command for rebuilding.
    - Cleaned up project structure to enhance maintainability and reduce clutter.
    
    This commit streamlines the project by removing unnecessary files and updating configurations for improved development workflow.

--- electron-client/src/api/models/RoomParticipantsResponse.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { RoomParticipant } from './RoomParticipant';
-export type RoomParticipantsResponse = {
-    room_id?: string;
-    participants?: Array<RoomParticipant>;
-};
